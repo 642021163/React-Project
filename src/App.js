0c8aba7ff941d5495fe906a73f55fb9b07533738
642021163 <akkarachai003@gmail.com>
2024-08-09 19:02:56 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { Route, Routes  } from 'react-router-dom';
+import Login from './Component/Login';
+import Home from './Component/Home';
+import Upload from './Component/Upload';
+import Register from './Component/Register';
+import EditDocument from './Component/EditDocument';
+import HomeStatus from './Component/HomeStatus';
+import UserCreate from './Component/UserCreate';
+import UserUpdate from './Component/UserUpdate';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/upload' element={<Upload />} />
+        <Route path="/edit/:id" element={<EditDocument />} />
+        <Route path='/home1' element={<HomeStatus />} />
+        <Route path='/create' element={<UserCreate />} />
+        <Route path='/update/:id' element={<UserUpdate />} />
+
+      </Routes>
     </div>
   );
 }
